e2a5fb87a535d5726d76bfe0383f3c1b79d92ec7
Graham Flaspoehler <grahamflas@gmail.com>
2019-10-07 12:09:52 -0400

    Done.

--- application.rb
+++ application.rb
@@ -4,7 +4,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, my name is Graham Flaspoehler"
+    resp.write "Hello, my name is Graham Flaspoehler."
     resp.finish
   end
   
